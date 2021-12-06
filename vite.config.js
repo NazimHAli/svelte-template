bdd71cf416f6439e3d816cbc721a5f3e085eb6bb
Nazim <nazimali@gmail.com>
2021-12-05 23:35:05 -0500

    REV: Disable https for CI

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,12 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { resolve } from "path";
 
+
 export default defineConfig({
     plugins: [svelte()],
     resolve: {
         alias: {
             "@": resolve(".", "src"),
         },
-    },
-    server: {
-        https: true,
-    },
+    }
 });
