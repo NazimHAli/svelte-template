293f8a6fb6bfb80331a1eb88fc1032c4a6033502
Nazim <nazimali@gmail.com>
2021-12-05 23:47:47 -0500

    REV: Remove unused package

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,11 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { resolve } from "path";
 
-
 export default defineConfig({
     plugins: [svelte()],
     resolve: {
         alias: {
             "@": resolve(".", "src"),
         },
-    }
+    },
 });
