3123a25b0441858f07fbae9d36653b3ebe18f076
Nazim Ali <nazimali@gmail.com>
2024-09-06 08:54:49 -0400

    chore: major upgrades - tailwind, postcss

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
 module.exports = {
+    content: ["./src/**/*.{html,js,svelte,ts}"],
     purge: false,
-    darkMode: false, // or 'media' or 'class'
+    darkMode: "media", // or 'media' or 'class'
     theme: {
         extend: {},
     },
